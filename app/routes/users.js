a214318106263c83f9488242e749e1b8644408d4
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-07-20 09:58:18 -0300

    create router for user

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,9 +1,62 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const User = require('../models/user')
+
+require('dotenv').config();
+
+router.post('/register',async (req,res) =>{
+  const { name, email, password} = req.body;
+  const user = new User({name, email, password})
+
+  try {
+    await user.save();
+    res.status(201).json({
+      message: 'User created successfully',
+      user
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: error.message 
+    });
+  }
+})
+
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+      let user = await User.findOne({ email });
+
+      if (!user) {
+          return res.status(400).json({ error: 'Invalid credentials' });
+      }
+
+      const isMatch = await user.isCorrectPassword(password);
+
+      if (!isMatch) {
+          return res.status(400).json({ error: 'Invalid credentials' });
+      }
+
+      const token = user.generateAuthToken();
+
+      res.json({
+          message: 'User logged in successfully',
+          user: {
+              name: user.name,
+              email: user.email,
+              _id: user._id,
+              created_at: user.created_at,
+              updated_at: user.updated_at
+          },
+          token
+      });
+
+  } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server error');
+  }
 });
+
 
 module.exports = router;
