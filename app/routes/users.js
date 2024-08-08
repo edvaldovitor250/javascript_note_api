ae0597abeea4752be0a96af6865e9e07b1dbaacb
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-08-08 18:10:22 -0300

    Update API

--- app/routes/users.js
+++ app/routes/users.js
@@ -22,6 +22,44 @@ router.post('/register',async (req,res) =>{
   }
 })
 
+router.put('/', withAuth, async function(req, res) {
+  const { name, email } = req.body;
+
+  try {
+    var user = await User.findOneAndUpdate(
+      {_id: req.user._id}, 
+      { $set: { name: name, email: email}}, 
+      { upsert: true, 'new': true }
+    )
+    res.json(user);
+  } catch (error) {
+    res.status(401).json({error: error});
+  }
+});
+
+router.put('/password', withAuth, async function(req, res) {
+  const { password } = req.body;
+
+  try {
+    var user = await User.findOne({_id: req.user._id})
+    user.password = password
+    user.save()
+    res.json(user);
+  } catch (error) {
+    res.status(401).json({error: error});
+  }
+});
+
+router.delete('/', withAuth, async function(req, res) {
+  try {
+    let user = await User.findOne({_id: req.user._id });
+    await user.delete();
+    res.json({message: 'OK'}).status(201);
+  } catch (error) {
+    res.status(500).json({error: error});
+  }
+});
+
 router.post('/login', async (req, res) => {
   const { email, password } = req.body;
 
